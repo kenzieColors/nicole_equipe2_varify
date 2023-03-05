90ce98dd9f6a8caa496e001222cc929c782c0ba9
Vitor Hugo Mendes <vitor_hugorj@hotmail.com>
2023-03-05 12:41:31 -0300

    configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-function App() {
-  return <div className="App"></div>;
-}
+import Router from "./routes";
+
+const App = () => <Router />;
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { UserProvider } from "./providers/UserContext/UserContext";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
