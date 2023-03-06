d266555db8ff0aad09520fabf20c8edfc054d1d7
Vitor Hugo Mendes <vitor_hugorj@hotmail.com>
2023-03-06 16:08:32 -0300

    fix: usercontext bugs, material ui dependences

--- src/providers/UserContext/UserContext.tsx
+++ src/providers/UserContext/UserContext.tsx
@@ -1,17 +1,23 @@
 import { createContext, useState } from "react";
 
-interface iUserContext {}
+interface iUserContext {
+  user: iUser | null;
+}
 
 interface iUserContextProps {
   children: React.ReactNode;
 }
 
-interface iUser {}
+interface iUser {
+  id: number;
+}
 
 export const UserContext = createContext({} as iUserContext);
 
 export const UserProvider = ({ children }: iUserContextProps) => {
   const [user, setUser] = useState<iUser | null>(null);
 
-  return <UserContext.Provider value={}></UserContext.Provider>;
+  return (
+    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+  );
 };
