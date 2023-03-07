95da5e613802c04e201ddf37a7ed379ff7ca5082
liniker <linikers@hotmail.com>
2023-03-07 11:13:48 -0300

    teste

--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -1,8 +1,8 @@
-import { styled } from 'styled-components';
+import { styled } from "styled-components";
 
 export const FieldSet = styled.fieldset`
   width: 100%;
   display: flex;
   flex-direction: column;
-  gap: 6px;
+  gap: 8px;
 `;
