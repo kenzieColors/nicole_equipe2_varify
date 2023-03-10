1292da949e177ac3da758f5ce6ad300d71c97760
suenefuzzo <suenequeiroz@hotmail.com>
2023-03-10 08:49:31 -0300

    fix: style login page

--- src/pages/LoginPage/style.ts
+++ src/pages/LoginPage/style.ts
@@ -1,27 +1,27 @@
 import styled from "styled-components";
 
-const StyledRegisterPage = styled.main`
+const StyledLoginPage = styled.main`
   height: 100vh;
   width: 100vw;
-  background-color: #212529;
 
-  .container__registerForm{
+  .container__loginForm {
     border: 2px solid var(--color-brand-4);
     margin: 15px;
     padding: 20px;
     border-radius: var(--radius-default);
-    
+
     h2{
       color: var(--grey-500);
+      text-align: center;
     }
   }
 
   @media (min-width: 600px){
-    .container__registerForm{
+    .container__loginForm{
       max-width: 600px;
       margin: 30px auto;
     }
   }
-`
+`;
 
-export default StyledRegisterPage
+export default StyledLoginPage;
