7a7350025e538dd587862e51fbb5b5ee1dbd7318
Thiago Almeida <dev.thiagoalmeida@gmail.com>
2023-03-13 16:17:42 -0300

    Feat: updated vars card

--- src/components/UserSavedVars/style.ts
+++ src/components/UserSavedVars/style.ts
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
-const StyledUserSavedVars = styled.section`
+const StyledUserSavedVars = styled.ul`
   border: 2px solid var(--color-brand-4);
   margin: 15px;
   padding: 20px;
   border-radius: var(--radius-default);
+
+  min-width: 260px;
 `;
 
 export default StyledUserSavedVars;
--- src/pages/DashboardPage/style.ts
+++ src/pages/DashboardPage/style.ts
@@ -6,10 +6,16 @@ const StyledDashboard = styled.div`
 
   height: 100vh;
   margin: 0 auto;
-  padding: 20px;
 
   display: flex;
   flex-direction: column;
+
+  h2 {
+    color: var(--grey-0);
+  }
+
+  @media (max-width: 769px) {
+  }
 `;
 
 export default StyledDashboard;
