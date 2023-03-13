888e004bf67d19a49360497e948624420dad7219
liniker <linikers@hotmail.com>
2023-03-13 14:10:51 -0300

    atualização css landing

--- src/style/globalStyle.ts
+++ src/style/globalStyle.ts
@@ -73,7 +73,6 @@ export const GlobalStyle = createGlobalStyle`
     background-color: #212529;
     font-family: 'Inter', sans-serif;
     overflow-x: hidden;
-    /* overflow-y: hidden; */
     height: 100vh;
   }
 
