0a83b65907004cf05c9c89a659a2207de6b3217a
Vitor Hugo Mendes <vitor_hugorj@hotmail.com>
2023-03-14 03:34:17 -0300

    fix:header children type

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,11 @@
 import { LogoLink, TopHeader, TopLink } from "./style";
 import varify from "../../assets/varify.png";
 
-const Header = ({ children }) => {
+interface IHeader {
+  children: React.ReactNode;
+}
+
+const Header = ({ children }: IHeader) => {
   return <TopHeader>{children}</TopHeader>;
 };
 
